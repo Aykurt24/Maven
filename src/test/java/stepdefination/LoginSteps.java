0ae7bcf72731a5726613c075b5a163ad4ac89eb9
Aykurt24 <doganaykurt@gmail.com>
2020-07-27 19:43:15 -0400

    Maven

--- src/test/java/stepdefination/LoginSteps.java
+++ src/test/java/stepdefination/LoginSteps.java
@@ -6,14 +6,19 @@ public class LoginSteps {
 
     @Given("^Navigate to website$")
     public void navigate_to_website()  {
+        System.out.println("navigate to website");
     }
 
     @Given("^Enter the username and password$")
     public void enter_the_username_and_password()  {
+        System.out.println("Enter the username and password");
+
     }
 
     @Given("^User should login successfully$")
     public void user_should_login_successfully() {
+        System.out.println("User should login successfully");
+
     }
 
 
